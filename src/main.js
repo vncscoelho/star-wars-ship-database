9c5fecc27f245d720c4af7e5588f6170823cd01a
Vinicius <vncs.coelho@gmail.com>
2021-01-24 12:17:01 -0300

    Lint config files

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { swapi } from './plugins/http'
 
-createApp(App).provide("swapi", swapi).mount("#app")
+createApp(App).provide('swapi', swapi).mount('#app')
