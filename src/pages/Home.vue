155a537f7270e4af73f0eb97686c252d4b3449e3
Vinicius <vncs.coelho@gmail.com>
2021-01-24 12:17:44 -0300

    Add starship fetch method

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,15 +7,25 @@
       minlength="10"
     >
     <button>GO</button>
+    <starship-card
+      v-for="(item, index) in data.starships"
+      :key="`starship-${index}`"
+      :data="item"
+    />
   </main>
 </template>
 
 <script setup>
-import { reactive, computed } from 'vue'
+import { reactive, computed, inject } from 'vue';
+import StarshipCard from '../components/StarshipCard.vue';
+
+const swapi = inject('swapi');
 
 const data = reactive({
-  targetDistance: 1000
+  targetDistance: 1000,
+  starships: []
 });
+
 const formattedDistance = computed({
   get() {
     return `${data.targetDistance} MGLT`;
@@ -24,7 +34,19 @@ const formattedDistance = computed({
     let newValue = value.match(/\d/g) || [];
     data.targetDistance = +newValue.join``;
   }
-})
+});
+
+
+const getAllStarships = (page = 1) => swapi(`/starships/?page=${page}`)
+  .then(response => {
+    const { results, next } = response.data;
+    data.starships.push(...results);
+    if (next) {
+      getAllStarships(++page);
+    }
+  });
+
+  getAllStarships();
 </script>
 
 <style>
