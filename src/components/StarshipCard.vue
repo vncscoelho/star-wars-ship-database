eddda84bc001cbd557f2fc5bf0456e7a87c785c9
Vinicius <vncs.coelho@gmail.com>
2021-01-24 12:17:17 -0300

    Add starship component

---
+++ src/components/StarshipCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="starship-card">
+    <template v-if="data.name">
+      {{ data.name }}
+    </template>
+    <template v-else>
+      ...
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({ 
+  data: {
+    type: Object,
+    default: () => {}
+  } 
+});
+</script>
+
+<style>
+
+</style>
