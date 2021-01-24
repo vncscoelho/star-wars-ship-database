100d52dbd938271d74d7c588a834d2072750eef8
Vinicius <vncs.coelho@gmail.com>
2021-01-24 11:11:21 -0300

    Add home component

---
+++ src/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h2>Starship Distance Calculator</h2>
+    <input
+      v-model="formattedDistance"
+      type="text"
+      minlength="10"
+    >
+    <button>GO</button>
+  </main>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue'
+
+const data = reactive({
+  targetDistance: 1000
+});
+const formattedDistance = computed({
+  get() {
+    return `${data.targetDistance} MGLT`;
+  },
+  set(value) {
+    let newValue = value.match(/\d/g) || [];
+    data.targetDistance = +newValue.join``;
+  }
+})
+</script>
+
+<style>
+
+</style>
